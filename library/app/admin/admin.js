fc1690f4f8ca2d611bf3f2aa99672bb0fdcf0ddf
AnaRadmanovic22 <anaradmanovic21@gmail.com>
2025-05-11 16:48:09 +0200

    Pocetni kod implementiran

---
+++ library/app/admin/admin.js
@@ -0,0 +1,12 @@
+'use strict'
+
+class Book{
+    constructor(id,name,date,url,description,popularity){
+        this.id = id;
+        this.name=name;
+        this.date=date;
+        this.url=url;
+        this.description=description;
+        this.popularity=popularity;
+    }
+}
