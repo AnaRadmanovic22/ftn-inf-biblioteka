c11da39bf787f4d0adc9d5c660a31e0f90965292
AnaRadmanovic22 <anaradmanovic21@gmail.com>
2025-05-12 12:37:59 +0200

    Implementiran pocetni kod za drugi deo zadatka

---
+++ library/app/lending/lending.js
@@ -0,0 +1,12 @@
+'use strict'
+
+class Book{
+    constructor(id,title,date,url,description,popularity){
+        this.id = id;
+        this.title=title;
+        this.date=date;
+        this.url=url;
+        this.description=description;
+        this.popularity=popularity;
+    }
+}
